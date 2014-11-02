5224c37c332272897023e930bd2f2b5408957da8
anibal copitan <detops.tk@gmail.com>
2014-11-02 05:19:22 -0500

    config module video

--- application/config/assets.php
+++ application/config/assets.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['assets_url'] = 'assets/';
+$config['assets_url'] = 'http://localhost/www.subingleslyrics.com/assets/';
 
 /* End of file assets.php */
 /* Location: ./application/config/assets.php */
---
+++ application/modules/video/controllers/video.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Video extends MY_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *      http://example.com/index.php/skeleton
+     *  - or -
+     *      http://example.com/index.php/skeleton/index
+     *  - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/skeleton/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        echo "index."; die;
+    }
+
+  
+}
+
+/* End of file skeleton.php */
+/* Location: ./application/modules/skeleton/controllers/skeleton.php */
