6158a25a3d1540168573b08444991b85cb06c5d3
anibal copitan <detops.tk@gmail.com>
2014-11-19 13:01:44 -0500

    update static URL

--- application/config/assets.php
+++ application/config/assets.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['assets_url'] = 'http://localhost/www.subingleslyrics.com/assets/';
+$config['assets_url'] = 'assets/';
 
 
 // 01 - config addional by library composer
