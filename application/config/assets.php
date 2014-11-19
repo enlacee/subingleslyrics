9e4745d8f9902c26b6bbde560aa381b258fdc371
anibal copitan <detops.tk@gmail.com>
2014-11-19 13:12:42 -0500

    update assets in AC2

--- application/config/assets.php
+++ application/config/assets.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['assets_url'] = 'assets/';
+$config['assets_url'] = 'http://54.69.199.90/assets/';
 
 
 // 01 - config addional by library composer
